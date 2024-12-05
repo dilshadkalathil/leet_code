b490c1a4bbdd21ff12f19ad699be44ec0c97080c
muhammed dilshad <dilshadkalathil61@gmail.com>
2024-12-05 17:33:17 +0530

    Time: 365 ms (95.25%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,15 @@
+SELECT 
+    MAX(num) AS num
+FROM 
+    (SELECT 
+         num
+     FROM 
+         MyNumbers
+     GROUP BY 
+         num
+     HAVING 
+         COUNT(*) = 1) AS single_numbers;
+
+
+
+
